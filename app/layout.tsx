bc73e7c295ce44be9daa05de8fefa7d3e45e30d3
ibrahim-qordhawi <ibrahim.qordhawi@idstar.co.id>
2023-05-09 13:39:04 +0700

    navbar

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import '@/styles/globals.css';
 import { AddressBar } from '@/ui/address-bar';
 import Byline from '@/ui/byline';
-import { GlobalNav } from '@/ui/global-nav';
+import { Navbar } from '@/ui/navbar';
 import { Metadata } from 'next';
 
 export const metadata: Metadata = {
@@ -20,20 +20,20 @@ export default function RootLayout({
   return (
     <html lang="id" className="[color-scheme:dark]">
       <body className="bg-gray-1100 overflow-y-scroll bg-[url('/grid.svg')] pb-36">
-        <GlobalNav />
+        <Navbar />
 
-        <div className="lg:pl-72">
+        <div className="py-32">
           <div className="mx-auto max-w-4xl space-y-8 px-2 pt-20 lg:py-8 lg:px-8">
-            <div className="rounded-lg bg-vc-border-gradient p-px shadow-lg shadow-black/20">
+
+            {/* <div className="rounded-lg bg-vc-border-gradient p-px shadow-lg shadow-black/20">
               <div className="rounded-lg bg-black">
                 <AddressBar />
               </div>
-            </div>
+            </div> */}
 
             <div className="rounded-lg bg-vc-border-gradient p-px shadow-lg shadow-black/20">
               <div className="rounded-lg bg-black p-3.5 lg:p-6">{children}</div>
             </div>
-            <Byline className="fixed sm:hidden" />
           </div>
         </div>
       </body>
---
+++ ui/navbar.tsx
@@ -0,0 +1,36 @@
+'use client';
+
+import useScroll from "@/lib/hooks/use-scroll";
+import Image from "next/image";
+import Link from "next/link";
+import { NextLogo } from '@/ui/next-logo';
+
+export function Navbar() {
+  const scrolled = useScroll(50);
+
+  return (
+    <div
+      className={`fixed top-0 w-full ${scrolled
+        ? "border-b border-gray-600 bg-black/10 backdrop-blur-xl"
+        : "bg-black/0"
+        } z-30 transition-all`}
+    >
+      <div className="mx-5 flex h-16 max-w-screen-xl items-center justify-between xl:mx-auto">
+        <Link
+          href="/"
+          // className="group flex w-full items-center gap-x-2.5"
+          className="group flex items-center font-display text-2xl gap-x-2.5"
+          onClick={close}
+        >
+          <div className="h-7 w-7 rounded-full border border-white/30 group-hover:border-white/50">
+            <NextLogo />
+          </div>
+
+          <h3 className="font-semibold tracking-wide text-gray-400 group-hover:text-gray-50">
+            Pondok Pelajar
+          </h3>
+        </Link>
+      </div>
+    </div>
+  )
+}
