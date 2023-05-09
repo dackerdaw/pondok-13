f6d9c53198595ae720aa5e4198a52db42f13c71b
ibrahim-qordhawi <ibrahim.qordhawi@idstar.co.id>
2023-05-09 20:12:55 +0700

    add link to lessons

--- app/bidang/[subjectSlug]/[courseSlug]/[unitSlug]/[moduleSlug]/page.tsx
+++ app/bidang/[subjectSlug]/[courseSlug]/[unitSlug]/[moduleSlug]/page.tsx
@@ -1,5 +1,6 @@
-import { getSubject } from '@/app/api/subjects/getSubjects';
+import { getSubject, getSubjects } from '@/app/api/subjects/getSubjects';
 import { SkeletonCard } from '@/ui/skeleton-card';
+import Link from 'next/link';
 
 export default async function Page({
   params,
@@ -7,15 +8,34 @@ export default async function Page({
   params: { moduleSlug: string };
 }) {
   const module = await getSubject({ slug: params.moduleSlug });
+  const lessons = await getSubjects({ parent: params.moduleSlug })
 
   return (
     <div className="space-y-4">
       <h1 className="text-xl font-medium text-gray-400/80">{module.name}</h1>
 
-      <div className="grid grid-cols-1 gap-6 lg:grid-cols-3">
-        {Array.from({ length: module.count }).map((_, i) => (
-          <SkeletonCard key={i} />
-        ))}
+      <div className="space-y-10 text-white">
+        {lessons.map((lesson) => {
+          return (
+            <div className="grid grid-cols-1 gap-5 lg:grid-cols-2">
+              <Link
+                href={`/${lesson.slug}`}
+                key={lesson.name}
+                className="group block space-y-1.5 rounded-lg bg-gray-900 px-5 py-3 hover:bg-gray-800"
+              >
+                <div className="font-medium text-gray-200 group-hover:text-gray-50">
+                  {lesson.name}
+                </div>
+
+                {lesson.name ? (
+                  <div className="text-sm text-gray-400 line-clamp-3 group-hover:text-gray-300">
+                    {lesson.name}
+                  </div>
+                ) : null}
+              </Link>
+            </div>
+          );
+        })}
       </div>
     </div>
   );
