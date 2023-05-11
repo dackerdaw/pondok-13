7f05319bfd5ddbd230956f29a08dcab70edac8d3
ibrahim-qordhawi <ibrahim.qordhawi@idstar.co.id>
2023-05-11 12:59:57 +0700

    added all info

--- app/(learn)/[subjectSlug]/[courseSlug]/[unitSlug]/[moduleSlug]/[lessonSlug]/layout.tsx
+++ app/(learn)/[subjectSlug]/[courseSlug]/[unitSlug]/[moduleSlug]/[lessonSlug]/layout.tsx
@@ -17,6 +17,10 @@ export default async function Layout({
   params: { moduleSlug: string, unitSlug: string, courseSlug: string, subjectSlug: string };
 }) {
   const lessons = await getSubjects({ parent: params.moduleSlug })
+  const module = await getSubject({ slug: params.moduleSlug })
+  const unit = await getSubject({ slug: params.unitSlug })
+  const course = await getSubject({ slug: params.courseSlug })
+  const subject = await getSubject({ slug: params.subjectSlug })
 
   return (
 
@@ -36,33 +40,48 @@ export default async function Layout({
           <div className="col-span-full lg:col-span-1">
             <div className="rounded-lg bg-vc-border-gradient p-px shadow-lg shadow-black/20">
               <div className="rounded-lg bg-black p-3.5 lg:p-6">
-                <div className="space-y-4">
-                  {lessons.map((lesson) => {
-                    return (
-                      <div className="grid grid-cols-1 gap-5" key={lesson.slug}>
-                        <Link
-                          href={`/${params.subjectSlug}/${params.courseSlug}/${params.unitSlug}/${params.moduleSlug}/${lesson.slug}`}
-                          key={lesson.name}
-                          className="group block space-y-1.5 rounded-lg bg-gray-900 px-5 py-3 hover:bg-gray-800"
-                        >
-                          <div className="font-medium text-gray-200 group-hover:text-gray-50">
-                            {lesson.name}
-                          </div>
 
-                          {lesson.name ? (
-                            <div className="text-sm text-gray-400 line-clamp-3 group-hover:text-gray-300">
-                              {lesson.name}
-                            </div>
-                          ) : null}
-                        </Link>
+
+                <div className="space-y-8">
+                  <h3 className="text-xl font-medium text-gray-300">{course.name}</h3>
+                  <h4 className="text-base font-medium text-gray-300">{unit.name}</h4>
+
+                  <div className="space-y-10 text-white">
+
+                    <div className="space-y-5">
+
+                      <div className="text-xs font-semibold uppercase tracking-wider text-gray-400">
+                        {module.name}
                       </div>
-                    );
-                  })}
+                      {lessons.map((lesson) => {
+                        return (
+                          <div className="grid grid-cols-1 gap-5" key={lesson.slug}>
+                            <Link
+                              href={`/${params.subjectSlug}/${params.courseSlug}/${params.unitSlug}/${params.moduleSlug}/${lesson.slug}`}
+                              key={lesson.name}
+                              className="group block space-y-1.5 rounded-lg bg-gray-900 px-5 py-3 hover:bg-gray-800"
+                            >
+                              <div className="font-medium text-gray-200 group-hover:text-gray-50">
+                                {lesson.name}
+                              </div>
+
+                              {lesson.name ? (
+                                <div className="text-sm text-gray-400 line-clamp-3 group-hover:text-gray-300">
+                                  {lesson.name}
+                                </div>
+                              ) : null}
+                            </Link>
+                          </div>
+                        );
+                      })}
+                    </div>
+                  </div>
                 </div>
+
               </div>
             </div>
           </div>
-          
+
           {children}
 
 
