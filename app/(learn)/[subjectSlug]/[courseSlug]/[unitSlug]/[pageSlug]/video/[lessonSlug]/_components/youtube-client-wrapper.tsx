68bc85a490a83210144989115f434f8b7820acfe
dackerdaw <dacker.daw@gmail.com>
2023-07-01 16:04:07 +0700

    finished transcripts

--- app/(learn)/[subjectSlug]/[courseSlug]/[unitSlug]/[pageSlug]/video/[lessonSlug]/_components/youtube-client-wrapper.tsx
+++ app/(learn)/[subjectSlug]/[courseSlug]/[unitSlug]/[pageSlug]/video/[lessonSlug]/_components/youtube-client-wrapper.tsx
@@ -3,7 +3,7 @@
 import YouTube, { YouTubePlayer, YouTubeProps } from 'react-youtube';
 import 'react-lite-youtube-embed/dist/LiteYouTubeEmbed.css';
 import styles from './styles.module.css'
-import { useState, useRef } from 'react';
+import { useState, useRef, useEffect } from 'react';
 import { Transcript, Video } from '@/app/api/videos/video';
 import { Button } from '@material-tailwind/react';
 
@@ -18,6 +18,7 @@ export default function YoutubeClientWrapper({
   // const [currentTime, setCurrentTime] = useState(0);
   const playerRef = useRef<YouTubePlayer>(); // Reference to the YouTube player instance
   const timerRef = useRef<any>(); // Reference to the timer interval
+  const transcriptContainerRef = useRef<HTMLDivElement>(null);
 
   const seekToTime = (time: number) => {
     if (playerRef.current) {
@@ -27,7 +28,8 @@ export default function YoutubeClientWrapper({
   };
 
   // param is set to any instead of number because getCurrentTime
-  // returns a promise<number> for some reason
+  // returns a promise<number> for some reason. this is just 
+  // typescript issue
   const updateCurrentTranscript = (currentTime: any) => {
     const transcriptIndex = transcripts.findIndex(
       (transcript) => transcript.start <= currentTime && transcript.end >= currentTime
@@ -71,11 +73,20 @@ export default function YoutubeClientWrapper({
     }
   }
 
+  useEffect(() => {
+    if (transcriptContainerRef.current) {
+      const activeTranscriptElement = transcriptContainerRef.current.children[currentTranscriptIndex] as HTMLElement;
+      if (activeTranscriptElement) {
+        const topPos = activeTranscriptElement.offsetTop;
+        console.log(activeTranscriptElement)
+        console.log(topPos)
+        transcriptContainerRef.current.scrollTop = topPos - 192;
+      }
+    }
+  }, [currentTranscriptIndex]);
+
   return (
     <>
-
-
-
       <div className="col-span-full lg:col-span-2 space-y-8">
 
         <div className="rounded-lg bg-vc-border-gradient p-px shadow-lg shadow-black/20">
@@ -90,7 +101,6 @@ export default function YoutubeClientWrapper({
                   <h2 className="text-xl font-medium text-gray-400/80">{video.name}</h2>
 
                   <div className="space-y-10 text-white">
-
 
                     <YouTube
                       videoId={video.external_video_id}
@@ -115,18 +125,16 @@ export default function YoutubeClientWrapper({
         </div>
       </div>
 
-      <div className="col-span-full lg:col-span-1 overflow-y-auto max-h-96">
+      <div className="col-span-full lg:col-span-1 ">
         <div className="rounded-lg bg-vc-border-gradient p-px shadow-lg shadow-black/20">
           <div className="rounded-lg bg-black p-3.5 lg:p-6">
 
             <div className="space-y-8">
               <h2 className="text-xl font-medium text-gray-300">Transcript</h2>
 
-              <ul>
+              <div className="overflow-y-auto max-h-96 relative scroll-smooth" ref={transcriptContainerRef}>
                 {transcripts.map((transcript, index) => (
-                  <li
-                    key={index}
-                  >
+                  <div key={index}>
                     <Button
                       onClick={() => {
                         setCurrentTranscriptIndex(index);
@@ -137,9 +145,11 @@ export default function YoutubeClientWrapper({
                       {transcript.startISO}
                     </Button>
                     {transcript.text}
-                  </li>
+                  </div>
                 ))}
-              </ul>
+              </div>
+
+
             </div>
 
           </div>
