147799d1846377fde5a42609a9cb5af0af8abb3c
dackerdaw <dacker.daw@gmail.com>
2023-07-03 14:53:33 +0700

    commented out breadcrumbs

--- app/(discover)/layout.tsx
+++ app/(discover)/layout.tsx
@@ -30,11 +30,11 @@ export default async function RootLayout({
         <div className="py-32">
           <div className="mx-auto max-w-4xl space-y-8 px-2 pt-20 lg:py-8 lg:px-8">
 
-            <div className="rounded-lg bg-vc-border-gradient p-px shadow-lg shadow-black/20">
+            {/* <div className="rounded-lg bg-vc-border-gradient p-px shadow-lg shadow-black/20">
               <div className="rounded-lg bg-black">
                 <AddressBar />
               </div>
-            </div>
+            </div> */}
 
             <div className="rounded-lg bg-vc-border-gradient p-px shadow-lg shadow-black/20">
               <div className="rounded-lg bg-black p-3.5 lg:p-6">
--- app/(learn)/layout.tsx
+++ app/(learn)/layout.tsx
@@ -26,11 +26,11 @@ export default function RootLayout({
           <div className="space-y-8 px-2 pt-20 lg:py-8 lg:px-8">
 
 
-            <div className="rounded-lg bg-vc-border-gradient p-px shadow-lg shadow-black/20">
+            {/* <div className="rounded-lg bg-vc-border-gradient p-px shadow-lg shadow-black/20">
               <div className="rounded-lg bg-black">
                 <AddressBar />
               </div>
-            </div>
+            </div> */}
 
             {children}
 
