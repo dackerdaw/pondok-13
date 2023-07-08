63e94d2c410b1c7fb587458eebdac0ddf98c0026
dackerdaw <dacker.daw@gmail.com>
2023-07-08 11:01:10 +0700

    deleted db copy

---
+++ pocketbase/pb_migrations/1688787950_updated_assessment_items.js
@@ -0,0 +1,47 @@
+migrate((db) => {
+  const dao = new Dao(db)
+  const collection = dao.findCollectionByNameOrId("i2pirwhc761ciqd")
+
+  // update
+  collection.schema.addField(new SchemaField({
+    "system": false,
+    "id": "jcdvpvwm",
+    "name": "problem_type_parent",
+    "type": "relation",
+    "required": false,
+    "unique": false,
+    "options": {
+      "collectionId": "92q3kwccymbu1fv",
+      "cascadeDelete": false,
+      "minSelect": null,
+      "maxSelect": 1,
+      "displayFields": [
+        "slug"
+      ]
+    }
+  }))
+
+  return dao.saveCollection(collection)
+}, (db) => {
+  const dao = new Dao(db)
+  const collection = dao.findCollectionByNameOrId("i2pirwhc761ciqd")
+
+  // update
+  collection.schema.addField(new SchemaField({
+    "system": false,
+    "id": "jcdvpvwm",
+    "name": "problem_type_parent",
+    "type": "relation",
+    "required": false,
+    "unique": false,
+    "options": {
+      "collectionId": "92q3kwccymbu1fv",
+      "cascadeDelete": false,
+      "minSelect": null,
+      "maxSelect": 1,
+      "displayFields": []
+    }
+  }))
+
+  return dao.saveCollection(collection)
+})
