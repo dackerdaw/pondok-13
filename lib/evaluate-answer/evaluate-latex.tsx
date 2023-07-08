4cea1175c81d5888855153ed5d9f32e25b0f6d95
dackerdaw <dacker.daw@gmail.com>
2023-07-08 12:51:49 +0700

    commented out the console log

--- lib/evaluate-answer/evaluate-latex.tsx
+++ lib/evaluate-answer/evaluate-latex.tsx
@@ -10,10 +10,10 @@ export interface MathInputAnswer {
 export default function evaluateMathInput(answer: MathInputAnswer) {
     const ce = new ComputeEngine();
 
-    console.log(`mathjson input: `)
-    console.log(answer.mathJSONInput)
-    console.log(`mathjson correct answer: `)
-    console.log(answer.mathJSONCorrectAnswer)
+    // console.log(`mathjson input: `)
+    // console.log(answer.mathJSONInput)
+    // console.log(`mathjson correct answer: `)
+    // console.log(answer.mathJSONCorrectAnswer)
     // ce.tolerance = answer.tolerance;
     if (!answer.mathJSONInput) {
         return {
@@ -28,10 +28,10 @@ export default function evaluateMathInput(answer: MathInputAnswer) {
             message: "Jawaban kamu belum valid. Pastikan jawabanmu sudah lengkap."
         };
     }
-    console.log(`boxed input: `)
-    console.log(boxedInput)
-    console.log(`is boxed canonical: `)
-    console.log(boxedInput.isCanonical)
+    // console.log(`boxed input: `)
+    // console.log(boxedInput)
+    // console.log(`is boxed canonical: `)
+    // console.log(boxedInput.isCanonical)
 
     let canonicalAnswer = ce.box(answer.mathJSONCorrectAnswer);
     if (!canonicalAnswer.isValid) {
@@ -41,13 +41,13 @@ export default function evaluateMathInput(answer: MathInputAnswer) {
         }
     }
 
-    console.log(`correct boxed answer: `)
-    console.log(canonicalAnswer)
-    console.log(`is correct boxed canonical: `)
-    console.log(canonicalAnswer.isCanonical)
+    // console.log(`correct boxed answer: `)
+    // console.log(canonicalAnswer)
+    // console.log(`is correct boxed canonical: `)
+    // console.log(canonicalAnswer.isCanonical)
 
     const passed = boxedInput.isEqual(canonicalAnswer)
-    console.log(`is input correct: ${passed}`)
+    // console.log(`is input correct: ${passed}`)
     if (!passed) {
         return {
             code: 460,
@@ -56,16 +56,16 @@ export default function evaluateMathInput(answer: MathInputAnswer) {
     }
     
     const latexInput = boxedInput.latex
-    console.log(`latexed input: `)
-    console.log(latexInput)
+    // console.log(`latexed input: `)
+    // console.log(latexInput)
 
     let latexAnswer = canonicalAnswer.latex
-    console.log(`correct latex answer: `)
-    console.log(latexAnswer)
+    // console.log(`correct latex answer: `)
+    // console.log(latexAnswer)
     
     
     const same = latexInput === latexAnswer
-    console.log(`is input same: ${same}`)
+    // console.log(`is input same: ${same}`)
 
     // results in db should be written as mathjson expression (rational)
     // not pure numerical
